520838cd99c64e73a7c5b15c3dd4285a42607cbe
wz930206 <super__wu@126.com>
2020-03-05 21:46:53 +0800

    初始化

---
+++ blog/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import { Button } from 'antd'
+const Home = () => (
+  <>
+    <Head>
+      <title>home</title>
+    </Head>
+    <div>
+      <Button type="dashed">Dashed</Button>
+    </div>
+  </>
+)
+
+export default Home
