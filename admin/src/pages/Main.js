19be9c6bf754cff3c27f0d2f33a9216df5ab2b21
wuzhao <wuzhao@youlanw.com>
2020-03-07 16:57:42 +0800

    后台管理系统

--- admin/src/pages/Main.js
+++ admin/src/pages/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router,Route} from 'react-router-dom';
 import Login from './Login';
 import AdminIndex from './AdminIndex';
-import AddArticle from './AddArticle'
 function Main() {
     return (
         <div>
